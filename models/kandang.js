57532ffa0fc0397b60ec33f19b41923f2d9fc9e1
naxsyambis <naxsyambis@gmail.com>
2025-11-04 14:05:14 +0700

    mendefenisikan table database di dalam models kandang.js dan menyimpan konfigurasi database

---
+++ models/kandang.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const Kandang = sequelize.define('Kandang', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        Nama_hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Nama_petugas: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Usia_hewan: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        Jenis_hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Tahun_lahir: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    })
+}
