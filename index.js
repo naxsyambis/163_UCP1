1b0d38fcfc85bd8b91d19d9d079137177994cb0b
naxsyambis <naxsyambis@gmail.com>
2025-11-04 14:12:50 +0700

    membuat POST dan GET di dahulukan POST karena database belum memiliki data

--- index.js
+++ index.js
@@ -4,3 +4,38 @@ const app = express();
 const db = require('./models');
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log(`Server Started`);
+    })
+})
+    .catch((err) => {
+        console.log(err);
+    })
+
+app.post('/kandang', async (req, res) => {
+    const data = req.body;
+    try{
+        const kandang = await db.Kandang.create(data);
+        res.send(kandang);
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+})
+
+app.get('/kandang', async (req, res) => {
+    const data = req.body;
+    try{
+        const kandang = await db.Kandang.findAll();
+        res.send(kandang);
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+})
